201fda6f18603de2da5914a2c95cb07e0c25d1fc
mohdtausif <mohd.tausif@perennialsys.com>
2023-08-19 11:25:03 +0530

    add .env file

--- src/components/googleMap/index.jsx
+++ src/components/googleMap/index.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import {GoogleMap, LoadScript, Marker} from '@react-google-maps/api'
+// import dotenv from 'dotenv'
 
 import './index.css'
+// dotenv.config()
 
 const GoogleMapContainer = ({lat, lng}) => {
+  const GOOGLE_API_KEY = process.env.REACT_APP_GOOGLE_API_KEY
+  console.log("GOOGLE_API_KEY", GOOGLE_API_KEY)
   const mapStyles = {
     height: '100%',
     width: '100%',
@@ -16,7 +20,7 @@ const GoogleMapContainer = ({lat, lng}) => {
   };
   return (
     <div className="google-map-container">
-      <LoadScript googleMapsApiKey="AIzaSyAZQiUfQ8gsxo5a5XVvV69BE02buyVQLKE">
+      <LoadScript googleMapsApiKey={GOOGLE_API_KEY}>
         <GoogleMap
           mapContainerStyle={mapStyles}
           zoom={10}
